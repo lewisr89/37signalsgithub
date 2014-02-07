7087ae67f56422c743e78ae011fd40a8281810df
lewisr89 <lewisr89@gmail.com>
2014-02-06 20:22:54 -0500

    Final Fixes Feb 6 2014

--- application.js
+++ application.js
@@ -4,58 +4,40 @@
 $("#basecamp").hover(function(){
       $("#headertext").hide();
       $("#basecamprollover").show();
+      $("#leftarrow1 img").show();
+
 });
 $("#basecamp").mouseleave(function(){
     $("#basecamprollover").hide();
     $("#headertext").show();
+    $("#leftarrow1 img").hide();
 });
 
 //Hover Over HiRise Box
 $("#highrise").hover(function(){
       $("#headertext").hide();
       $("#hiriserollover").show();
+      $("#leftarrow2 img").show();
 });
 $("#highrise").mouseleave(function(){
     $("#hiriserollover").hide();
     $("#headertext").show();
+    $("#leftarrow2 img").hide();
 });
 
 //Hover Over Campfire Box
 $("#campfire").hover(function(){
       $("#headertext").hide();
       $("#campfirerollover").show();
+      $("#rightarrow img").show();
 });
 $("#campfire").mouseleave(function(){
     $("#campfirerollover").hide();
     $("#headertext").show();
+    $("#rightarrow img").hide();
 });
 
-//Hover Over Basecamp Box Arrow Appear
-$("#basecamp").hover(function(){
-      $("#leftarrow1").show();
-});
 
-$("#basecamp").mouseleave(function(){
-    $("#leftarrow1").hide();
-  });
-
-//Hover Over Hirise Box Arrow Appear
-$("#highrise").hover(function(){
-      $("#leftarrow2").show();
-});
-
-$("#highrise").mouseleave(function(){
-    $("#leftarrow2").hide();
-  });
-
-//Hover Over Campfire Box Arrow Appear
-$("#campfire").hover(function(){
-      $("#rightarrow").show();
-});
-
-$("#campfire").mouseleave(function(){
-    $("#rightarrow").hide();
-  });
 
 
 });
